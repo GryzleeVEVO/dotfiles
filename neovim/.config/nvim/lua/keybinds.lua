34a7405c42f0d96f9b9bad94db3b1fba4d6ddfec
Dorian Wozniak <dorian.wozniak@gmail.com>
2025-05-12 17:19:41 +0200

    LSP keybinds

--- neovim/.config/nvim/lua/keybinds.lua
+++ neovim/.config/nvim/lua/keybinds.lua
@@ -1,5 +1,7 @@
 local map = vim.keymap.set
 local g = vim.g
+local au = vim.api.nvim_create_autocmd
+local ag = vim.api.nvim_create_augroup
 
 g.mapleader = " "
 g.localmapleader = " "
@@ -12,3 +14,27 @@ map({ "n", "v", "i" }, "<down>", "", { desc = "Disable arrows" })
 map("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlight" })
 
 map("n", "<C-q>", "<C-w><C-q>", { desc = "Close window" })
+
+-- When an LSP is attached, add this keybinds
+local lsp_gr = ag("user-lsp-attach", { clear = true })
+
+au({ "LspAttach" }, {
+  desc = "Set up keybindings after attaching an LSP",
+  group = lsp_gr,
+  callback = function(ev)
+    map({ "n" }, "<F2>", vim.lsp.buf.rename, {
+      buffer = ev.buf,
+      desc = "[LSP] Rename",
+    })
+
+    map({ "n", "x" }, "<leader>a", vim.lsp.buf.code_action, {
+      buffer = ev.buf,
+      desc = "[LSP] Code Action",
+    })
+
+    map({ "n", "x" }, "<leader>d", vim.diagnostic.open_float, {
+      buffer = ev.buf,
+      desc = "[LSP] Show diagnostic pop-up",
+    })
+  end,
+})
