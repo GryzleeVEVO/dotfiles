7a8f17c8711e53776029b1f94d63c776c8b383af
Dorian Wozniak <dorian.wozniak@gmail.com>
2025-05-14 18:09:36 +0200

    Telescope keybinds

--- neovim/.config/nvim/lua/plugins/completions.lua
+++ neovim/.config/nvim/lua/plugins/completions.lua
@@ -10,7 +10,15 @@ return {
     --- @module "blink.cmp"
     --- @type blink.cmp.Config
     opts = {
+      keymap = {
+        preset = "default",
+
+        ["<C-j>"] = { "select_next", "fallback" },
+        ["<C-k>"] = { "select_prev", "fallback" },
+      },
+
       signature = {
+        -- Show signature while filling a functiofunction
         enabled = true,
         window = {
           show_documentation = false,
