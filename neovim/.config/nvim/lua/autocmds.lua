422fa976525587ef048f4cfa340de453e6719640
GryzleeVEVO <dorian.wozniak@gmail.com>
2025-05-16 13:30:32 +0200

    More LSP stuff

--- neovim/.config/nvim/lua/autocmds.lua
+++ neovim/.config/nvim/lua/autocmds.lua
@@ -1,8 +1,10 @@
 local au = vim.api.nvim_create_autocmd
+local auclr = vim.api.nvim_clear_autocmds
 local ag = vim.api.nvim_create_augroup
 local cmd = vim.cmd
 
 local qol_gr = ag("qol-fixes", { clear = true })
+local lsp_gr = ag("lsp-attach-setup", { clear = true })
 
 au({ "BufEnter" }, {
   desc = "Do not insert comment prefix on line break",
@@ -42,4 +44,40 @@ au({ "BufRead", "BufNewFile" }, {
   group = qol_gr,
   pattern = "*docker-compose*.{yml,yaml}",
   command = "set filetype=yaml.docker-compose",
+})
+
+au({ "LspAttach" }, {
+  desc = "Set up stuff on a newly attached LSP",
+  group = lsp_gr,
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    local highlightSupported = client
+      and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, ev.buf)
+
+    if highlightSupported then
+      local lsp_hl_gr = ag("lsp-highlight", { clear = false })
+
+      au({ "CursorHold", "CursorHoldI" }, {
+        desc = "Highlight references of symbol in cursor",
+        group = lsp_hl_gr,
+        buffer = ev.buf,
+        callback = vim.lsp.buf.document_highlight,
+      })
+
+      au({ "CursorMoved", "CursorMovedI" }, {
+        desc = "Highlight",
+        group = lsp_hl_gr,
+        buffer = ev.buf,
+        callback = vim.lsp.buf.clear_references,
+      })
+
+      au({ "LspDetach" }, {
+        desc = "Clean up stuff after detaching an LSP",
+        callback = function(ev2)
+          vim.lsp.buf.clear_references()
+          auclr({ group = "lsp-highlight", buffer = ev2.buf })
+        end,
+      })
+    end
+  end,
 })
