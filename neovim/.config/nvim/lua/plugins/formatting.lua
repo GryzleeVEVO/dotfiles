58b50836cf229c75eab3098c46dc234b09bd8a6c
Dorian Wozniak <dorian.wozniak@gmail.com>
2025-06-17 13:07:04 +0200

    Changed nvim theme, added autoclose and autotag plugins

--- neovim/.config/nvim/lua/plugins/formatting.lua
+++ neovim/.config/nvim/lua/plugins/formatting.lua
@@ -5,24 +5,42 @@ return {
     "stevearc/conform.nvim",
 
     event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
+    cmd = { "ConformInfo", "Format" },
 
-    opts = {
-      formatters_by_ft = tools.conform_formatters,
+    config = function()
+      local conform = require("conform")
 
-      default_format_opts = {
-        lsp_format = "fallback",
-        stop_after_first = true,
-      },
+      conform.setup({
+        formatters_by_ft = tools.conform_formatters,
 
-      format_on_save = function()
-        if vim.g.enable_autoformat then
-          return { timeout_ms = 500 }
+        default_format_opts = {
+          lsp_format = "fallback",
+          stop_after_first = true,
+        },
+
+        format_on_save = function()
+          if vim.g.enable_autoformat then
+            return { timeout_ms = 500 }
+          end
+        end,
+
+        notify_on_error = true,
+      })
+
+      vim.api.nvim_create_user_command("Format", function(args)
+        local range = nil
+
+        if args.count ~= -1 then
+          local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+          range = {
+            start = { args.line1, 0 },
+            ["end"] = { args.line2, end_line:len() },
+          }
         end
-      end,
 
-      notify_on_error = true,
-    },
+        conform.format({ async = true, range = range })
+      end, { range = true })
+    end,
   },
   {
     "NMAC427/guess-indent.nvim",
