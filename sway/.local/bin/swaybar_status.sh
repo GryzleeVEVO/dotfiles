df8930e7c9d9fd7d1e7d065990cf32f1453d51a8
GryzleeVEVO <dorian.wozniak@gmail.com>
2025-11-04 12:02:39 +0100

    sway fixes

--- sway/.local/bin/swaybar_status.sh
+++ sway/.local/bin/swaybar_status.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while sleep 0.01; do
+while sleep 0.2; do
 	vol="$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | cut -d ' ' -f 3)"
 
 	if [[ -z $vol ]]; then
@@ -8,6 +8,11 @@ while sleep 0.01; do
 	fi
 
 	bat="$(cat /sys/class/power_supply/BAT0/capacity)%"
+
+	if [[ $(cat /sys/class/power_supply/BAT0/status) = "Charging" ]]; then
+		bat+=" Charging"
+	fi
+
 	date="$(date +'%Y-%m-%d %X')"
 	echo "VOL ${vol} | BAT ${bat} | ${date}"
 done
