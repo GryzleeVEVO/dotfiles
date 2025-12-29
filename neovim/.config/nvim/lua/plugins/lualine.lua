6d2d1fedf68154cb308b95473dc049086fa5a674
GryzleeVEVO <dorian.wozniak@gmail.com>
2025-12-29 17:18:55 +0100

    lualine

--- neovim/.config/nvim/lua/plugins/lualine.lua
+++ neovim/.config/nvim/lua/plugins/lualine.lua
@@ -6,5 +6,19 @@ return {
     "nvim-tree/nvim-web-devicons", -- Dependency
   },
 
-  opts = {},
+  opts = {
+    options = {
+      component_separators = "",
+      section_separators = { left = "", right = "" },
+    },
+
+    sections = {
+      lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+      lualine_b = { "filename" },
+      lualine_c = { "branch", "diff" },
+      lualine_x = { "diagnostics" },
+      lualine_y = { "filetype", "progress" },
+      lualine_z = { { "location", separator = { right = "" }, left_padding = 2 } },
+    },
+  },
 }
