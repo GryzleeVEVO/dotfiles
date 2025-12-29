8943c531530e156b58dc651518888a1a1bd5c507
GryzleeVEVO <dorian.wozniak@gmail.com>
2025-12-29 17:20:03 +0100

    lualine 2.0

--- neovim/.config/nvim/lua/plugins/lualine.lua
+++ neovim/.config/nvim/lua/plugins/lualine.lua
@@ -16,7 +16,7 @@ return {
       lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
       lualine_b = { "filename" },
       lualine_c = { "branch", "diff" },
-      lualine_x = { "diagnostics" },
+      lualine_x = { "diagnostics", "lsp_status" },
       lualine_y = { "filetype", "progress" },
       lualine_z = { { "location", separator = { right = "" }, left_padding = 2 } },
     },
